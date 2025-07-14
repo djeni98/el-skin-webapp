3c23bb1d82e21989e114ce8fb0bd3971a72065e9
DJENIFER RENATA PEREIRA <drpereira@mppr.mp.br>
2025-07-14 13:16:14 -0300

    Refaz Header igual ao da aula

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,52 @@
+import React, { useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSearch, faCartShopping } from '@fortawesome/free-solid-svg-icons';
+import './Header.css';
+ 
+function Header() {
+  const [textoBusca, setTextoBusca] = useState('valor inicial do texto');
+  function handleOnChange(e: React.ChangeEvent<HTMLInputElement>) {
+    setTextoBusca(e.target.value);
+  }
+
+  function onClickSearch(): void {
+    console.log(`Você pesquisou por: ${textoBusca}`);
+  }
+
+  return (
+    <header className="header">
+      <div className="header-top">
+        <div className="container">
+          <div className="logo">
+            <span>AL SKIN</span>
+          </div>
+ 
+          <div className="search-bar">
+            <input
+              type="text"
+              placeholder="O que você está procurando?"
+              className="search-input"
+              onChange={handleOnChange}/>
+            <button className="search-button" onClick={onClickSearch} >
+              <FontAwesomeIcon icon={faSearch} />
+            </button>
+          </div>
+ 
+          <div className="header-actions">
+            <button className="cart-button">
+              <FontAwesomeIcon icon={faCartShopping} />
+            </button>
+          </div>
+        </div>
+      </div>
+ 
+      <nav className="header-nav">
+        <h6>{textoBusca}</h6>
+      </nav>
+    </header>
+  );
+}
+ 
+export default Header;
+ 
+ 
