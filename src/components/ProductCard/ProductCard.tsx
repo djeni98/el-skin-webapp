8c91028a45b5f333eab12221d7db170738e8e84d
DJENIFER RENATA PEREIRA <drpereira@mppr.mp.br>
2025-07-15 13:47:08 -0300

    Refatora ProductCard para receber produto como props

--- src/components/ProductCard/ProductCard.tsx
+++ src/components/ProductCard/ProductCard.tsx
@@ -1,20 +1,44 @@
 import "./ProductCard.css";
-import img1 from "../../assets/img1.png"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faShoppingBag } from "@fortawesome/free-solid-svg-icons";
 
-function ProductCard() {
+export interface IProduct {
+  id: string;
+  name: string;
+  description: string;
+  price: number;
+  image: string;
+  tags: Array<{
+    label: string;
+    backgroundColor: 'pink' | 'seal';
+  }>;
+}
+
+export interface IProductCardProps {
+  product: IProduct
+}
+
+function ProductCard(props: Readonly<IProductCardProps>) {
+  const product = props.product;
+
+  function formatPrice(price: number): string {
+    return `R$ ${price.toFixed(2).replace('.', ',')}`;
+  }
+
   return (
     <div className="product-card">
-      <img className="img-product" src={img1} alt="img1" />
-      <span className="product-title">Protetor solar AL SUN</span>
-      <span className="product-description">alta proteção e pele luminosa sem grude nem pele cinzenta</span>
+      <div className="img-product">
+        <img src={product.image} alt={product.name} />
+      </div>
+      <span className="product-title">{product.name}</span>
+      <span className="product-description">{product.description}</span>
       <div className="tags-list-product">
-        <span className="tag-product bg-seal">proteção</span>
-        <span className="tag-product bg-pink">rosto</span>
+        { product.tags.map((tag) => (
+          <span className={`tag-product bg-${tag.backgroundColor}`} key={tag.label}>{tag.label}</span>
+        ))}
       </div>
       <div className="buy-section">
-        <span className="buy-price">R$ 79,90</span>
+        <span className="buy-price">{formatPrice(product.price)}</span>
         <button className="buy-button">
           comprar 
           <FontAwesomeIcon icon={faShoppingBag} />
--- src/components/ProductList/ProductList.tsx
+++ src/components/ProductList/ProductList.tsx
@@ -1,19 +1,37 @@
-import ProductCard from "../ProductCard/ProductCard"
+import ProductCard, { IProduct } from "../ProductCard/ProductCard"
+import img1 from "../../assets/img1.png";
 import "./ProductList.css"
 
 function ProductList() {
+  const productX: IProduct = {
+    id: "protetor",
+    name: "Protetor solar AL SUN",
+    description: "alta proteção e pele luminosa sem grude nem pele cinzenta",
+    price: 79.90,
+    image: img1,
+    tags: [
+      { label: "proteção", backgroundColor: "seal" },
+      { label: "rosto", backgroundColor: "pink" },
+    ]
+  }
+  
+  const productList = [
+    { ...productX, id: "item-1"},
+    { ...productX, id: "item-2"},
+    { ...productX, id: "item-3"},
+    { ...productX, id: "item-4"},
+    { ...productX, id: "item-5"},
+    { ...productX, id: "item-6"},
+    { ...productX, id: "item-7"},
+  ]
+
   return (
     <section className="product-list container">
       <h2 className="product-list-title">nossos queridinhos estão aqui</h2>
       <div className="product-items">
-        <ProductCard />
-        <ProductCard />        
-        <ProductCard />        
-        <ProductCard />
-        <ProductCard />        
-        <ProductCard />
-        <ProductCard />        
-        <ProductCard />
+        { productList.map((item) => (
+          <ProductCard product={item} key={item.id} />
+        ))}
       </div>
     </section>
   )
