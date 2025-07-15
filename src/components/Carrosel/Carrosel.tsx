2a38dc5060f5bdc0ed21282420e1f138c07c0e46
DJENIFER RENATA PEREIRA <drpereira@mppr.mp.br>
2025-07-15 16:34:38 -0300

    Cria esqueleto do Carrosel, sem estado

--- src/components/Carrosel/Carrosel.tsx
+++ src/components/Carrosel/Carrosel.tsx
@@ -1,10 +1,43 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import img1 from '../../assets/img1.png';
 import './Carrosel.css';
+import { faArrowLeft, faArrowRight } from '@fortawesome/free-solid-svg-icons';
 
 function Carrosel() {
-  const bgCarrosel = {backgroundImage: `url(${img1})`};
+  const item = {
+    subtitle:'confira nossa linha',
+    title:'corporal',
+    description: 'com benefícios além da hidratação',
+    backgroundImage: img1
+  };
+
+  function getStyleForImgCarrosel(img: string) {
+    return {backgroundImage: `url(${img})`};
+  }
+
   return (
-    <div className="img-carrosel" style={bgCarrosel} />
+    <div className="img-carrosel" style={getStyleForImgCarrosel(item.backgroundImage)}>
+      <div className="container flex-1">
+        <button className='carrosel-button'>
+          <FontAwesomeIcon icon={faArrowLeft} />
+        </button>
+        <div className='carrosel-text'>
+          <span className='carrosel-subtitle'>{item.subtitle}</span>
+          <h1 className='carrosel-title'>{item.title}</h1>
+          <p className='carrosel-description'>{item.description}</p>
+          <button className='carrosel-buy-button'>
+            comprar agora
+            <FontAwesomeIcon icon={faArrowRight} />
+          </button>
+        </div>
+
+        <div className='flex-1'>
+        </div>
+        <button className='carrosel-button'>
+          <FontAwesomeIcon icon={faArrowRight} />
+        </button>
+      </div>
+    </div>
   );
 }
 
