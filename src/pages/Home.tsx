b663d9359ff43d13547f007f6dfdbef353f11aea
DJENIFER RENATA PEREIRA <drpereira@mppr.mp.br>
2025-07-15 17:22:31 -0300

    Adicionar react-router e configura Home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,17 +1,13 @@
 import Carrosel from '../components/Carrosel/Carrosel';
-import Footer from '../components/Footer/Footer';
-import Header from '../components/Header/Header';
 import ProductList from '../components/ProductList/ProductList';
 import PromoBanner from '../components/PromoBanner/PromoBanner';
 
 function Home() {
   return (
     <>
-      <Header /> 
       <PromoBanner />
       <Carrosel />
       <ProductList />
-      <Footer />
     </>
   );
 }
---
+++ src/routes.tsx
@@ -0,0 +1,16 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
+import Home from './pages/Home';
+
+export default function AppRouter() {
+  return (
+    <Router>
+      <Header />
+      <Routes>
+        <Route path='/' element={<Home />} />
+      </Routes>
+      <Footer />
+    </Router>
+  );
+}
