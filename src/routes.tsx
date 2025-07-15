e7aa9446843eca3f68b4ea94678420830d9a0b1a
DJENIFER RENATA PEREIRA <drpereira@mppr.mp.br>
2025-07-15 18:32:27 -0300

    Cria página Sobre

--- src/routes.tsx
+++ src/routes.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
 import Home from './pages/Home';
+import Sobre from './pages/Sobre/Sobre';
 
 export default function AppRouter() {
   return (
@@ -9,6 +10,7 @@ export default function AppRouter() {
       <Header />
       <Routes>
         <Route path='/' element={<Home />} />
+        <Route path='/quem-somos' element={<Sobre />} />
       </Routes>
       <Footer />
     </Router>
