fe70bd483c2b355265a17d18167461677195c6a5
DJENIFER RENATA PEREIRA <drpereira@mppr.mp.br>
2025-07-16 16:32:11 -0300

    Consome dados da api para o Carrosel

--- src/components/Carrosel/Carrosel.tsx
+++ src/components/Carrosel/Carrosel.tsx
@@ -1,10 +1,8 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import img1 from '../../assets/img1.png';
-import img2 from '../../assets/img2.png';
-import img3 from '../../assets/img3.png';
 import { useEffect, useState } from 'react';
 import './Carrosel.css';
 import { faArrowLeft, faArrowRight } from '@fortawesome/free-solid-svg-icons';
+import axios from 'axios';
 
 interface ICarouselItem {
   subtitle: string;
@@ -15,28 +13,7 @@ interface ICarouselItem {
 }
 
 function Carrosel() {
-  const items: ICarouselItem[] = [    {
-    subtitle:'confira nossa linha',
-    title:'corporal',
-    description: 'com benefícios além da hidratação',
-    backgroundImage: img1,
-    textPosition: 'left'
-  },
-  {
-    subtitle:'toda linha',
-    title:'anti-age',
-    description: 'use o cupom ANTIAGE15',
-    backgroundImage: img2,
-    textPosition: 'left'
-  },
-  {
-    subtitle:'',
-    title:'kits incríveis',
-    description: 'até 50% OFF',
-    backgroundImage: img3,
-    textPosition: 'right'
-  }];
-
+  const [items, setItems] = useState<ICarouselItem[]>([]);
   const [idxItemAtual, setIdxItemAtual] = useState(0);
 
   function previousItem() {
@@ -47,43 +24,64 @@ function Carrosel() {
     setIdxItemAtual((prevIdx) => (prevIdx === items.length - 1 ? 0 : prevIdx + 1));
   }
 
-  function getStyleForImgCarrosel(img: string) {
-    return {backgroundImage: `url(${img})`};
+  function requestCarroselItems() {
+    axios.get<ICarouselItem[]>('http://localhost:3001/carousel')
+      .then((response) => {
+        const data = response.data;
+        setItems(data);
+        console.log('API-Carousel - OK');
+      })
+      .catch((err) => {
+        console.log(`API-Carousel - Err : ${err}`);
+      });
   }
 
-  const item = items[idxItemAtual];
+  function getStyleForImgCarrosel(img: string) {
+    return { backgroundImage: `url(${img})` };
+  }
 
   useEffect(() => {
+    if (items.length === 0) { return; }
     const timer = setInterval(() => { nextItem(); }, 3000);
     return () => { clearInterval(timer); };
+  }, [items]);
+
+  useEffect(() => {
+    requestCarroselItems();
   }, []);
-  
+
+  const item = items.length > 0 ? items[idxItemAtual] : null;
+
   return (
-    <div className="img-carrosel" style={getStyleForImgCarrosel(item.backgroundImage)}>
-      <div className="container flex-1">
-        <button className='carrosel-button' onClick={previousItem}>
-          <FontAwesomeIcon icon={faArrowLeft} />
-        </button>
+    <>
+      {item &&
+        <div className="img-carrosel" style={getStyleForImgCarrosel(item.backgroundImage)}>
+          <div className="container flex-1">
+            <button className='carrosel-button' onClick={previousItem}>
+              <FontAwesomeIcon icon={faArrowLeft} />
+            </button>
 
-        { item.textPosition === 'right' && <div className='flex-1' /> }
+            {item.textPosition === 'right' && <div className='flex-1' />}
 
-        <div className='carrosel-text'>
-          <span className='carrosel-subtitle'>{item.subtitle}</span>
-          <h1 className='carrosel-title'>{item.title}</h1>
-          <p className='carrosel-description'>{item.description}</p>
-          <button className='carrosel-buy-button'>
-            comprar agora
-            <FontAwesomeIcon icon={faArrowRight} />
-          </button>
+            <div className='carrosel-text'>
+              <span className='carrosel-subtitle'>{item.subtitle}</span>
+              <h1 className='carrosel-title'>{item.title}</h1>
+              <p className='carrosel-description'>{item.description}</p>
+              <button className='carrosel-buy-button'>
+                comprar agora
+                <FontAwesomeIcon icon={faArrowRight} />
+              </button>
+            </div>
+
+            {item.textPosition === 'left' && <div className='flex-1' />}
+
+            <button className='carrosel-button'>
+              <FontAwesomeIcon icon={faArrowRight} onClick={nextItem} />
+            </button>
+          </div>
         </div>
-
-        { item.textPosition === 'left' && <div className='flex-1' /> }
-
-        <button className='carrosel-button'>
-          <FontAwesomeIcon icon={faArrowRight} onClick={nextItem} />
-        </button>
-      </div>
-    </div>
+      }
+    </>
   );
 }
 
