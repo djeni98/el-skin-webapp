9bc0c385872e067b5cf73b370705cf249baad930
DJENIFER RENATA PEREIRA <drpereira@mppr.mp.br>
2025-07-16 15:21:28 -0300

    Adiciona navegação que faltava

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -4,6 +4,13 @@ import { faSearch, faCartShopping } from '@fortawesome/free-solid-svg-icons';
 import './Header.css';
  
 function Header() {
+  const navLinks = [
+    { name: 'Categorias', href: '/categorias' },
+    { name: 'Tipo de pele', href: '/tipo-pele' },
+    { name: 'Necessidade', href: '/necessidade' },
+    { name: 'Ingredientes', href: '/ingredientes' },
+  ];
+
   const [textoBusca, setTextoBusca] = useState('valor inicial do texto');
   function handleOnChange(e: React.ChangeEvent<HTMLInputElement>) {
     setTextoBusca(e.target.value);
@@ -41,6 +48,17 @@ function Header() {
       </div>
  
       <nav className="header-nav">
+        <div className="container">
+          <div className='header-nav-itens'>
+            { navLinks.map((navLink) => (
+              <a className="header-nav-item" href={navLink.href} key={navLink.href} >{navLink.name}</a>
+            ))}
+          </div>
+
+          <div className='promo-link-container'>
+            <a className="promo-link" href="/kits-50-off">Kits 50% off</a>
+          </div>
+        </div>
       </nav>
     </header>
   );
