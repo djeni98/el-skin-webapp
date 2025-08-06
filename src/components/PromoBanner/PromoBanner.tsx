b17b77f2d89e86121905e3592a2a04c7ae695df4
DJENIFER RENATA PEREIRA <drpereira@mppr.mp.br>
2025-08-06 13:22:45 -0300

    Usa styled nos componentes Footer e PromoBanner

--- src/components/PromoBanner/PromoBanner.tsx
+++ src/components/PromoBanner/PromoBanner.tsx
@@ -1,15 +1,49 @@
-import './PromoBanner.css';
+import styled from 'styled-components';
 
 function PromoBanner() {
   return (
-    <div className="banner">
-      <div className="container banner-container">
-        <span className="titulo-banner">primeira compra?</span>
-        <span><span className="font-bold">R$25</span> OFF A PARTIR DE <span className="font-bold">R$200</span></span>
-        <span className="codigo-promo">PRIMEIRA25</span>
-      </div>
-    </div>
+    <Banner>
+      <BannerContainer>
+        <Title>primeira compra?</Title>
+        <span>
+          <BoldFont>R$25</BoldFont> OFF A PARTIR DE <BoldFont>R$200</BoldFont>
+        </span>
+        <CodigoPromo>PRIMEIRA25</CodigoPromo>
+      </BannerContainer>
+    </Banner>
   );
 }
+
+const Banner = styled.div`
+  background-color: #DC995E;
+  padding: 0.5rem;
+`;
+
+const BannerContainer = styled.div`
+  max-width: 1200px;
+  margin: 0 auto;
+  padding: 0 1rem;
+  display: flex;
+  align-items: center;
+
+  justify-content: left;
+  gap: 3rem;
+`;
+
+const Title = styled.span`
+  font-weight: 700;
+  color: #fff;
+`;
+
+const BoldFont = styled.span`
+  font-weight: 700;
+`;
+
+const CodigoPromo = styled.span`
+  padding: 2px 16px;
+  background-color: #fff;
+  border-radius: 8px;
+  font-weight: 700;
+`;
 
 export default PromoBanner;
