cc41b2fa13cbc0f471ab7da002fcfd0d88173aa3
DJENIFER RENATA PEREIRA <drpereira@mppr.mp.br>
2025-08-06 14:38:58 -0300

    Usa styled no componente Header e na página NotFound

--- src/pages/NotFound/NotFound.tsx
+++ src/pages/NotFound/NotFound.tsx
@@ -1,21 +1,50 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import './NotFound.css';
 import { faHome } from '@fortawesome/free-solid-svg-icons';
+import styled from 'styled-components';
 
 function NotFound() {
   return (
-    <section className='not-found-section container'>
-      <div className='not-fount-content'>
+    <NotFoundSection>
+      <div>
         <h2>Página não encontrada!</h2>
         <p>A página que está tentando acessar não existe.</p>
 
-        <a className="back-home-link" href='/'>
+        <BackHomeLink href='/'>
           <FontAwesomeIcon icon={faHome} />
           Voltar para o início
-        </a>
+        </BackHomeLink>
       </div>
-    </section>
+    </NotFoundSection>
   );
 }
+
+const NotFoundSection = styled.section`
+  max-width: 1200px;
+  margin: 0 auto;
+  padding: 0 1rem;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  margin-top: 4rem;
+  margin-bottom: 8rem;
+`;
+
+const BackHomeLink = styled.a`
+  background: #94426E;
+  color: white;
+  padding: 24px 60px;
+  border-radius: 8px;
+  font-size: 16px;
+  display: inline-flex;
+  align-items: center;
+  gap: 24px;
+  margin-top: 3rem;
+  text-decoration: none;
+
+  &:hover {
+    background: #662e4c;
+  }
+`;
 
 export default NotFound;
