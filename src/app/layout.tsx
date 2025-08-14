e7ac804a1df71932d876479bc5a15782977ee341
DJENIFER RENATA PEREIRA <drpereira@mppr.mp.br>
2025-08-14 13:28:07 -0300

    Migra Header

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,17 @@
-import type { Metadata } from 'next';
+'use client';
+
 import '../index.css';
 import 'normalize.css';
 
-import Footer from '../components/Footer/Footer';
 import { config } from '@fortawesome/fontawesome-svg-core';
 import '@fortawesome/fontawesome-svg-core/styles.css';
+
+import Footer from '../components/Footer/Footer';
+import Header from '../components/Header/Header';
+import { Provider } from 'react-redux';
+import { store } from '../store';
+
 config.autoAddCss = false;
- 
-export const metadata: Metadata = {
-  title: 'React App',
-  description: 'Web site created with Next.js.',
-};
  
 export default function RootLayout({
   children,
@@ -19,9 +20,17 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <title>AL SKIN</title>
+        <meta name="description" content="Sobre a AL SKIN" />
+        <link rel="icon" href="/favicon.ico" />
+      </head>
       <body>
-        <div id="root">{children}</div>
-        <Footer />
+        <Provider store={store}>
+          <Header />
+          <div id="root">{children}</div>
+          <Footer />
+        </Provider>
       </body>
     </html>
   );
